276f860520f06bbabf4dc47cd528a067518f3d78
albireo3754 <starpro123@naver.com>
2024-01-06 01:57:22 +0900

    Init Project

---
+++ src/main/kotlin/com/albireo3754/pickbangg/PickbanggApplication.kt
@@ -0,0 +1,11 @@
+package com.albireo3754.pickbangg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PickbanggApplication
+
+fun main(args: Array<String>) {
+	runApplication<PickbanggApplication>(*args)
+}
---
+++ src/test/kotlin/com/albireo3754/pickbangg/PickbanggApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.albireo3754.pickbangg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PickbanggApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
